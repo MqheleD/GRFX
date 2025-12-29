4613587a19ad82a06b90c9da0824d6e1a2bb008c
MqheleD <ndabedd@gmail.com>
2025-12-29 20:57:29 +0200

    Header looks more header-y

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,21 @@ import { Link } from "react-router-dom"
 import "./styles/Header.css"
 
 export default function Header() {
-    return (
-        <div>
-            <div>Header</div>
-            <Link to="/">Home</Link>
-            <Link to="/Boards">Boards</Link>
-            <Link to="/Prints">Prints</Link>
-            <Link to="/Store">Store</Link>
-        </div>
-    )
+  return (
+    <header className="header">
+      <div className="logo">Header</div>
+
+      <nav className="navLinks">
+        <Link to="/">Home</Link>
+        <Link to="/Boards">Boards</Link>
+        <Link to="/Prints">Prints</Link>
+        <Link to="/Store">Store</Link>
+      </nav>
+
+      <div className="authButtons">
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </header>
+  )
 }
