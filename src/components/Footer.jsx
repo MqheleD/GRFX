8840084d07997f66b4f0c794e60c0ce1bf834446
MqheleD <ndabedd@gmail.com>
2026-01-05 17:41:29 +0200

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,11 @@ export default function Footer() {
         </nav>
 
         <div className="footerSocials">
+
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+          
         </div>
       </div>
 
